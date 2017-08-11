7a39ff8f0ca8effd4cbe4abb61f792e3a6bfa16b
Juan <juan@videogami.co>
2017-08-11 19:21:46 -0400

    Added Weather capabilities

--- server.js
+++ server.js
@@ -16,6 +16,20 @@ function askingTime(msg) {
   return msg.match(/time/)
 }
 
+function askingWeather(msg) {
+  return msg.match(/weather/)
+}
+
+function getWeather(callback) {
+  var request = require('request');
+  request.get("https://www.metaweather.com/api/location/4118/", function (error, response) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(response.body);
+      callback(data.consolidated_weather[0].weather_state_name);
+    }
+  })
+}
+
 io.on('connection', function (socket) {
   
   socket.on('message', function (msg) {
@@ -24,7 +38,11 @@ io.on('connection', function (socket) {
       io.emit('message', msg);
     } else if (askingTime(msg)) {
       io.emit('message', new Date);
-    } 
+    } else if (askingWeather(msg)) {
+      getWeather(function(weather) {
+        io.emit('message', weather)
+      })
+    }
   });
 });
 
